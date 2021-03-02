fc18ab2a2d86c4141e7429781fe6ae3b7a50c0e5
oldshensheep <29306733+oldshensheep@users.noreply.github.com>
2021-03-02 22:15:54 +0800

    存储日志，云函数代理

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 爬取`yande.re`网站上的所有图片  
 下载给定页数区间的图片，已经下载的会自动跳过  
-下载速度应该是很快的，破校园网，53代理破解再加clash代理可以跑到差不多50Mbs  
-建议安装好后看看下面的[提示](#提示)
+下载速度应该是很快的，破校园网，53 代理破解再加 clash 代理可以跑到差不多 50Mbs  
+下载后看看下面的[提示](#提示)，还有`config.ini`根据自身需求修改
 
 ## 环境要求
 
@@ -29,7 +29,7 @@ pip3 install requests
 ```
 
 打开 `config.ini` 按照文件里的提示配置, 国内要配置代理，支持 http 代理，不填用系统代理设置  
-如果打开了clash，v2ray等代理软件，并且开启系统代理应该不用填代理设置  
+如果打开了 clash，v2ray 等代理软件，并且开启系统代理应该不用填代理设置  
 这里列出了配置文件文件内容
 
 ```ini
@@ -44,16 +44,20 @@ end_page = 100
 ; 同时下载的线程数 默认10个线程
 max_workers = 10
 
-; 图片保存路径 默认为 yande.re, 不存在自动创建
-filepath = /mnt/d/Codespaces/Code/.io/yande.re
+; 图片保存文件夹 默认为 yande.re, 不存在自动创建
+filepath = yande.re
 
-; http代理 默认没有代理/系统代理
-proxy = http://localhost:7890
+; http代理 不填默认没有代理/系统代理
+; proxy = http://localhost:7890
+
+; 云函数代理 默认无代理 CloudFlare Workers 代理，不需要爬墙代理啦，每天10W次请求(UTC+0)
+; 部分地区/宽带 用这个可能还是下载不了或者很慢，建议使用http代理。
+; hproxy = https://http-proxy.oldshensheep.workers.dev
 
 ; 连接错误重试次数 默认 2
 max_retries = 2
 
-; 连接超时 默认 4
+; 连接超时 默认 4 出现Read timed out可尝试增大此值
 timeout = 5
 
 ; 画质 'original jpeg sample 画质由好到差
@@ -71,22 +75,22 @@ python3 run.py
 - 下载的文件会根据画质选项分类
 - 如果全部下载建议选择 `sample` 画质（空间多当我没说），`original` 画质平均文件大小在 3.5MB  
 ~~测试~~ **的时候不知不觉下载了 4000 多张差不多占了 17GB 的空间……**  
-***Be Careful!!!***  
-***我不知道图片内容!!!***  
-[original画质图片示例](https://files.yande.re/image/ff3bc48f06b4247a35d9553d08cccd9a/yande.re%20750007%20bikini%20garter%20genshin_impact%20mona_%28genshin_impact%29%20pn_%28wnsl216%29%20swimsuits%20underboob%20wet.png)  
-[jpeg画质图片示例](https://files.yande.re/jpeg/ff3bc48f06b4247a35d9553d08cccd9a/yande.re%20750007%20bikini%20garter%20genshin_impact%20mona_%28genshin_impact%29%20pn_%28wnsl216%29%20swimsuits%20underboob%20wet.jpg)  
-[sample画质图片示例](https://files.yande.re/sample/ff3bc48f06b4247a35d9553d08cccd9a/yande.re%20750007%20sample%20bikini%20garter%20genshin_impact%20mona_%28genshin_impact%29%20pn_%28wnsl216%29%20swimsuits%20underboob%20wet.jpg)
-- 如果你看到了似乎是一样的图片，那不是重复下载了，或者yande.re有重复的图片。（在这时你应当看看你周围有没有人然后……放大仔细观察有没有什么不妥）
+**_Be Careful!!!_**  
+**_我不知道图片内容!!!_**  
+[original 画质图片示例](https://files.yande.re/imageff3bc48f06b4247a35d9553d08cccd9ayandere%20750007%20bikini%20garter%20genshin_impact%20mona%28genshin_impact%29%20pn_%28wnsl26%29%20swimsuits%20underboob%20wet.png)  
+[jpeg 画质图片示例](https://files.yande.re/jpeg/ff3bc48f06b4247a35d9553d08cccd9ayandere%20750007%20bikini%20garter%20genshin_impact%20mona_%28genshin_impact%29%20pn_%28wnl26%29%20swimsuits%20underboob%20wet.jpg)  
+[sample 画质图片示例](https://files.yande.re/sample/ff3bc48f06b4247a35d9553d08cccd9ayandere%20750007%20sample%20bikini%20garter%20genshin_impact%20mona_%28genshin_impact%29%2pn%28wnsl216%29%20swimsuits%20underboob%20wet.jpg)
+- 如果你看到了似乎是一样的图片，那不是重复下载了，或者 yande.re 有重复的图片。（在这时你应当看看你周围有没有人然后……放大仔细观察有没有什么不妥）
 
 ## TODO
 
 1. ~~添加下载画质的选项~~
 2. 用数据库存储 id 的 tag 等信息，方便查询
-3. 添加按tag下载的功能
-4. 支持云函数代理 https://xxxxx.workers.dev/https://yande.re/post.json 用云函数代理，这样就可以省点流量了……(
-5. 存储log，更友好的爬取详细输出
+3. 添加按 tag 下载的功能
+4. ~~支持云函数代理 https://xxxxx.workers.dev/https://yande.re/post.json 用云函数代理，这样就可以省点流量了……(~~
+5. ~~存储 log，更友好的爬取详细输出~~
 6. 完善注释？
-7. 并行化请求图片url的过程
+7. 并行化请求图片 url 的过程
 8. add more todo
 
 ## License
--- run.py
+++ run.py
@@ -1,20 +1,26 @@
 import concurrent.futures
 import configparser
 import functools
+import logging
 import os
+from os.path import islink
+import queue
 import time
 
 import requests
 
 
 class YandeRe():
-
     _current_donwload = 0
-
     # 包含之前下载的
     _actual_download = 0
 
-    def __init__(self, end_page, proxies={'http': '', 'https': ''}, quality='original', limit=100, start_page=1, max_workers=10, max_retries=2, timeout=4, filepath='yande.re'):
+    _API_URL = 'https://yande.re/post.json'
+
+    _q = queue.Queue()
+
+    def __init__(self, end_page, proxies={'http': '', 'https': ''}, quality='original', limit=100, start_page=1,
+                 max_workers=10, max_retries=2, timeout=4, filepath='yande.re', hproxy=''):
         self.proxies = proxies
         self.quality = quality
         self.end_page = end_page
@@ -24,6 +30,8 @@ class YandeRe():
         self.max_retries = max_retries
         self.timeout = timeout
         self.filepath = filepath
+        if len(hproxy) != 0:
+            self._API_URL = f'{hproxy}/{self._API_URL}'
         self.executor = concurrent.futures.ThreadPoolExecutor(
             max_workers=self.max_workers)
         self.download_files = self.last_download_files()
@@ -33,6 +41,12 @@ class YandeRe():
             os.mkdir(subdir)
         self.filepath = subdir
         self.session = self.create_session()
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(filename='fuck.log', format='%(asctime)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s',
+                            level=logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        self.logger.addHandler(ch)
 
     def create_session(self):
         adapters = requests.adapters.HTTPAdapter(
@@ -45,29 +59,46 @@ class YandeRe():
             session.request, timeout=self.timeout)
         return session
 
-    def down_image(self, url, filename):
+    def down_image(self):
+        url, id = self._q.get()
+        filename = f'{id}{os.path.splitext(url)[1]}'
         if filename in self.download_files:
             self._actual_download += 1
-            print(f'{filename} already downloaded')
+            self.logger.info(f'{filename} already downloaded')
+            self._q.task_done()
             return
-        print(f"downloading {filename}")
-        image = self.session.get(url).content
+        self.logger.info(f'downloading {filename}')
+        try:
+            image = self.session.get(url).content
+        except Exception as e:
+            self.logger.info(f'{e}\ndownload {filename} failed')
+            print('请设置代理')
+            return
         with open(os.path.join(self.filepath, filename), 'wb') as f:
             f.write(image)
-            print(f"download {filename} succedded!")
-            self._current_donwload += 1
-            self._actual_download += 1
+        self.logger.info(f'download {filename} succeeded!')
+        self._current_donwload += 1
+        self._actual_download += 1
+        self._q.task_done()
 
     def get_img_urls(self, page):
-        post = self.session.get(
-            f'https://yande.re/post.json?limit={self.limit}&page={page}').json()
+        self.logger.info(f'get_img_urls {page}')
+        try:
+            post = self.session.get(
+                f'{self._API_URL}?limit={self.limit}&page={page}').json()
+        except Exception as e:
+            self.logger.info(f'{e}\nget_img_urls page{page} failed')
+            print('请设置代理')
+            return
         file_map = {
             'original': [(i['file_url'], i['id']) for i in post],
             'jpeg': [(i['jpeg_url'], i['id']) for i in post],
             'sample': [(i['sample_url'], i['id']) for i in post]
         }
         img_urls = file_map[self.quality]
-        return img_urls
+        for img_url in img_urls:
+            self._q.put(img_url)
+        self.logger.info(f'get page {page} img urls succeeded')
 
     def last_download_files(self):
         ids = list()
@@ -78,18 +109,33 @@ class YandeRe():
 
     def speed(self):
         while True:
-            # ad = self._actual_download
+            ad = self._actual_download
+            cd = self._current_donwload
             time.sleep(3)
-            print(f'actual download {self._actual_download}')
-            print(f'current download {self._current_donwload}')
+            if ad == self._actual_download and cd == self._current_donwload:
+                print('没动静')
+            else:
+                self.logger.info(f'actual download {self._actual_download}')
+                self.logger.info(f'current download {self._current_donwload}')
 
     def run(self):
         self.executor.submit(self.speed)
-        for i in range(self.start_page, self.end_page):
-            img_urls = self.get_img_urls(page=i)
-            result = {self.executor.submit(
-                self.down_image, i[0], f'{i[1]}{os.path.splitext(i[0])[1]}'): i for i in img_urls}
-            concurrent.futures.as_completed(result)
+        page_list = list(range(self.start_page, self.end_page + 1))
+        jobs = []
+        while True:
+            if len(page_list) <= 0:
+                break
+            if self._q.qsize() < 10:
+                self.get_img_urls(page_list.pop(0))
+
+            # 限制submit的数量，防止占用内存过大
+            jobing_num = len(jobs)
+            for job in jobs:
+                if job.done():
+                    jobs.remove(job)
+                    jobing_num -= 1
+            if self._q.qsize() > 0 and jobing_num <= 15:
+                jobs.append(self.executor.submit(self.down_image))
 
 
 def main():
@@ -101,22 +147,20 @@ def main():
         print('config.ini file not found')
         return
     config = config['DFF']
-    try:
-        start_page = int(config['start_page'])
-        end_page = int(config['end_page'])
-        max_workers = int(config['max_workers'])
-        filepath = config['filepath']
-        proxies = {
-            'http': config['proxy'],
-            'https': config['proxy']
-        }
-        max_retries = int(config['max_retries'])
-        timeout = float(config['timeout'])
-        quality = config['quality']
-    except Exception as e:
-        print(e)
+    start_page = int(config['start_page'])
+    end_page = int(config['end_page'])
+    max_workers = int(config['max_workers'])
+    filepath = config['filepath']
+    proxies = {
+        'http': config['proxy'],
+        'https': config['proxy']
+    }
+    max_retries = int(config['max_retries'])
+    timeout = int(config['timeout'])
+    quality = config['quality']
+    hproxy = config['hproxy']
     yandere = YandeRe(start_page=start_page, end_page=end_page, filepath=filepath, proxies=proxies,
-                      max_workers=max_workers,  max_retries=max_retries, timeout=timeout, quality=quality)
+                      max_workers=max_workers, max_retries=max_retries, timeout=timeout, quality=quality, hproxy=hproxy)
     yandere.run()
 
 
